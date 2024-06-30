4c02e534b5af964e5223c13d79e0d6a8d67e9101
cactus-spike1 <127032697+cactus-spike1@users.noreply.github.com>
2024-06-30 16:51:59 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ document.getElementById('webhookForm').addEventListener('submit', async function
   event.preventDefault();
   
   const formData = new FormData(this);
-  const response = await fetch('https://cactus_spike1.glitch.me/send-webhook', { // Измените URL
+  const response = await fetch('https://cactus_spike1.glitch.me/send-webhook', { 
     method: 'POST',
     body: formData,
   });
